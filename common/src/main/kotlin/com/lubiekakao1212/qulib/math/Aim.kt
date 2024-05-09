3c8faf7189253dac45f5353b177b1bb09b56b393
LubieKakao1212 <grzesio.go@gmail.com>
2024-05-09 17:38:06 +0200

    Improved Aim + Added angle utilities + version bump

--- common/src/main/kotlin/com/lubiekakao1212/qulib/math/Aim.kt
+++ common/src/main/kotlin/com/lubiekakao1212/qulib/math/Aim.kt
@@ -1,15 +1,39 @@
 package com.lubiekakao1212.qulib.math
 
+import net.minecraft.util.math.Direction
+import org.joml.Math
 import org.joml.Quaterniond
+import org.joml.Vector3d
+import kotlin.math.PI
 import kotlin.math.sqrt
 
-class Aim(var pitch : Double, var yaw : Double) {
+class Aim(pitchIn : Double, yawIn : Double) {
+
+    var pitch = pitchIn
+        set(value) {
+            field = Math.clamp(-PI / 2, PI / 2, value)
+        }
+
+    var yaw = yawIn
+        set(value) {
+            field = value.loopAngle()
+        }
+
+    fun stepPerAxis(target : Aim, maxDeltaPerAxis: Double, dst : Aim = this) : Aim {
+        return stepPerAxis(target, maxDeltaPerAxis, maxDeltaPerAxis, dst)
+    }
+
+    fun stepPerAxis(target : Aim, maxDeltaP: Double, maxDeltaY: Double, dst : Aim = this) : Aim {
+        dst.pitch = pitch.stepTo(target.pitch, maxDeltaP)
+        dst.yaw = yaw.stepToAngle(target.yaw, maxDeltaY)
+        return dst
+    }
 
     fun stepRaw(target : Aim, maxDelta : Double, dst : Aim = this) : Aim {
-        val p = target.pitch
-        val y = target.yaw
+        val dp = pitch.angleDistanceTo(target.pitch)
+        val dy = yaw - target.yaw
 
-        val l = sqrt(p*p + y*y);
+        val l = sqrt(dp * dp + dy * dy)
 
         if(l <= maxDelta) {
             dst.pitch = target.pitch
@@ -24,13 +48,21 @@ class Aim(var pitch : Double, var yaw : Double) {
     }
 
     fun lerpRaw(b : Aim, t : Double, dst : Aim = this) : Aim {
-        dst.pitch = lerp(pitch, b.pitch, t)
-        dst.pitch = lerp(yaw, b.yaw, t)
+        dst.pitch = lerpAngle(pitch, b.pitch, t)
+        dst.yaw = lerp(yaw, b.yaw, t)
         return dst
     }
 
     fun toQuaternion() : Quaterniond {
         return Quaterniond().aimRad(pitch, yaw)
+    }
+
+    fun toQuaternion(left: Vector3d, up: Vector3d) : Quaterniond {
+        return Quaterniond().aimRad(pitch, yaw, left, up)
+    }
+
+    fun toQuaternion(leftOrientation: Direction, upOrientation: Direction) : Quaterniond {
+        return Quaterniond().aimRad(pitch, yaw, leftOrientation, upOrientation)
     }
 
 }
--- common/src/main/kotlin/com/lubiekakao1212/qulib/math/MathUtil.kt
+++ common/src/main/kotlin/com/lubiekakao1212/qulib/math/MathUtil.kt
@@ -1,31 +1,108 @@
 package com.lubiekakao1212.qulib.math
 
+import net.minecraft.structure.OceanMonumentGenerator.DoubleYRoom
+import kotlin.math.*
+
 object Constants {
-    const val degToRad = (Math.PI / 180.0).toFloat()
+    const val degToRad = (PI / 180.0).toFloat()
     const val radToDeg = (180.0 / Math.PI).toFloat()
-    const val piHalf = Math.PI.toFloat() / 2f
-    const val pi = Math.PI.toFloat()
+    const val piHalf = PI.toFloat() / 2f
+    const val pi = PI.toFloat()
+
+    const val TAU = PI * 2
+    const val tau = pi * 2
 
     const val epsilon = 0.001
 }
-fun loop(a: Double, min: Double, max: Double): Double {
+
+fun loop(a: Double, min: Double, max: Double) : Double {
     val b = a - min
-    val c = b % (max - min)
+    val c = b.mod(max - min)
     return c + min
 }
 
-fun loop(a: Float, min: Float, max: Float): Float {
+fun loop(a: Float, min: Float, max: Float) : Float {
     val b = a - min
-    val c = b % (max - min)
+    val c = b.mod(max - min)
     return c + min
+}
+
+/**
+ *  @return value in range -pi, pi
+ */
+fun Double.loopAngle() : Double {
+    return this.mod(Constants.TAU) - PI
+}
+
+
+/**
+ * Assumes both a and b are in range -pi, pi
+ */
+fun angleDistance(a : Double, b : Double): Double {
+    val a1 = a.loopAngle()
+    val b1 = b.loopAngle()
+
+    val d1 = abs(b - a)
+    val d2 = abs(b1 - a1)
+    return min(d1, d2)
+}
+
+fun Double.angleDistanceTo(b : Double) : Double {
+    return angleDistance(this, b)
+}
+
+fun lerpAngle(a : Double, b : Double, t : Double) : Double {
+    val a1 = a.loopAngle()
+    val b1 = b.loopAngle()
+
+    val b2 = if(a1 > b1) b1 + Constants.TAU else b1 - Constants.TAU
+
+    val b3 = if(
+        abs(a1 - b1) <
+        abs(a1 - b2)) b1 else b2
+
+    return a1.lerpTo(b3, t).loopAngle()
+}
+
+fun stepAngle(a : Double, b : Double, maxDelta : Double) : Double {
+    val a1 = a.loopAngle()
+    val b1 = b.loopAngle()
+
+    val b2 = if(a1 > b1) b1 + Constants.TAU else b1 - Constants.TAU
+
+    val d1 = abs(a1 - b1)
+    val d2 = abs(a1 - b2)
+
+    val d = min(min(d1, d2), maxDelta)
+
+    return a + if(d1 < d2) d*sign(d1) else d*sign(d2)
+}
+
+fun Double.stepToAngle(b : Double, maxDelta : Double) : Double {
+    return stepAngle(this, b, maxDelta)
+}
+
+
+fun Double.stepTo(b : Double, maxDelta : Double) : Double {
+    val d1 = b - this
+    val d = min(abs(d1), maxDelta)
+    return this + sign(d1) * d
 }
 
 fun lerp(a: Double, b: Double, t: Double) : Double {
-    return  a + (b - a) * t;
+    return  a + (b - a) * t
 }
 
 fun lerpPrecise(a: Double, b: Double, t: Double) : Double {
     return (a * (1 - t)) + (b * t);
+}
+
+fun Double.lerpTo(b: Double, t: Double) : Double {
+    return  this + (b - this) * t
+}
+
+fun Double.lerpPreciseTo(b: Double, t: Double) : Double {
+    return (this * (1 - t)) + (b * t)
 }
 
 fun nonZeroSign(value: Double): Long {
